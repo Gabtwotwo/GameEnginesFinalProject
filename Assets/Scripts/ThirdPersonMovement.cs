196225998a3a926e6cb46c22b0d8190d3ac69295
Gabtwotwo <Gabtwotwo@github.com>
2021-02-26 21:24:45 -0500

    lotsa spaghetti

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,44 +1,135 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+
+    [SerializeField]
+    private InputActionReference movementControl;
+    [SerializeField]
+    private InputActionReference jumpControl;
+    [SerializeField]
+    private InputActionReference colorControl;
+    [SerializeField]
     private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float jumpHeight = 0.1f;
+    [SerializeField]
     private float gravityValue = -9.81f;
+    [SerializeField]
+    private float rotationSpeed = 4.00f;
+
+    private CharacterController controller;
+
+    [SerializeField]
+    private Vector3 playerVelocity;
+
+    [SerializeField]
+    private bool groundedPlayer;
+
+
+    private Transform cameraMain;
+    private int currentIndex;
+    private Animator animator;
+
+
+
+    [SerializeField]
+    public Material[] colours;
+
+
+    private void OnEnable()
+    {
+        movementControl.action.Enable();
+        jumpControl.action.Enable();
+        colorControl.action.Enable();
+
+
+
+    }
+
+    private void OnDisable()
+    {
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
+        colorControl.action.Disable();
+
+    }
 
     private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = gameObject.GetComponent<CharacterController>();
+        cameraMain = Camera.main.transform;
+        animator = GetComponentInChildren<Animator>();
+        currentIndex = 0;
     }
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
+
+        if (groundedPlayer)
+        {
+            playerVelocity.y = -1.0f;
+        }
+
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+
+        move = cameraMain.forward * move.z + cameraMain.right * move.x;
+        move.y = 0.0f;
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+
+        // Changes the height position of the player..
+        if (jumpControl.action.triggered && groundedPlayer)
+        {
+           
+              playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+                
+
+            
+        }
+
+        if (colorControl.action.triggered)
+        {
+            
+
+            gameObject.GetComponentInChildren<Renderer>().material = colours[0];
+            Debug.Log("Color Changed");
+        }
+
+ 
         controller.Move(playerVelocity * Time.deltaTime);
+
+        if(movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMain.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0.0f, targetAngle, 0.0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            animator.SetInteger("animState", 1);
+        }
+        else
+        {
+            
+             animator.SetInteger("animState", 0);
+            
+        }
+
+        if (!groundedPlayer)
+        {
+            animator.SetInteger("animState", 2);
+        }
+ 
+
+        groundedPlayer = controller.isGrounded;
+
+
     }
 }
