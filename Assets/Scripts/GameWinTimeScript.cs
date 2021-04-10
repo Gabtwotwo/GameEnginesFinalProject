a0fa3997c29610cf6b80b478d850d1e2c34d3a70
Gabtwotwo <gabrielville@hotmail.com>
2021-04-09 20:16:34 -0400

    Finished animations, fixed menus, added medal system, changed level, added music

--- Assets/Scripts/GameWinTimeScript.cs
+++ Assets/Scripts/GameWinTimeScript.cs
@@ -12,7 +12,19 @@ public class GameWinTimeScript : MonoBehaviour
     {
         int minutes = (int)PlayerPrefs.GetFloat("GameTimerMinutes");
 
-        timerText.text = "Time: " + minutes.ToString() + ":" + PlayerPrefs.GetFloat("GameTimerSeconds").ToString("f2");
+            if (PlayerPrefs.GetFloat("GameTimerSeconds") > 40)
+            {
+                timerText.text = "Time: " + minutes.ToString() + ":" + PlayerPrefs.GetFloat("GameTimerSeconds").ToString("f2") + " You've earned a bronze medal. There are better scores out there!";
+            }
+            else if (PlayerPrefs.GetFloat("GameTimerSeconds") > 30)
+            {
+                timerText.text = "Time: " + minutes.ToString() + ":" + PlayerPrefs.GetFloat("GameTimerSeconds").ToString("f2") + " You've earned a silver medal. Go for the gold!";
+            }
+            else if (PlayerPrefs.GetFloat("GameTimerSeconds") > 20)
+            {
+                timerText.text = "Time: " + minutes.ToString() + ":" + PlayerPrefs.GetFloat("GameTimerSeconds").ToString("f2") + " You've earned a gold medal. You've acheved the tippy top!";
+            }
+        
     }
 
     // Update is called once per frame
